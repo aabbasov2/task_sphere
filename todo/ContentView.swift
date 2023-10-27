87e50816c39aa73964e94397743c20ae33c91dd1
Aziz Abbasov <niyazioglu02@gmail.com>
2023-10-27 12:25:01 +0300

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Angelina Larionova on 27.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Angelina Larionova on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
